1afb6ab01cd8d5d19ff003a170e3b1eb15d3e4a7
Keith <turnerkj_git@protonmail.com>
2021-05-29 16:21:03 -0400

    Initial Commit

---
+++ src/main/java/com/keithturner1/springrestfulwebservice/SpringResTfulWebServiceApplication.java
@@ -0,0 +1,12 @@
+package com.keithturner1.springrestfulwebservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringResTfulWebServiceApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(SpringResTfulWebServiceApplication.class, args);
+  }
+}
---
+++ src/test/java/com/keithturner1/springrestfulwebservice/SpringResTfulWebServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.keithturner1.springrestfulwebservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringResTfulWebServiceApplicationTests {
+
+   @Test
+   void contextLoads() {
+   }
+
+}
